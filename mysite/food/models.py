4ff07187c924c8cba0924274a962a9a17acf41f3
Sajjad <sajjadzolghadr777@gmail.com>
2026-07-04 20:12:07 +0330

    feat: add image field to item model

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -5,6 +5,7 @@ class Item (models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
     description = models.TextField(max_length=500, blank=True)
+    image = models.CharField(max_length=500,default="https://www.thefuzzyduck.co.uk/wp-content/uploads/2024/05/image-coming-soon-placeholder-01-660x660.png")
     def __str__(self):
         return self.name
 

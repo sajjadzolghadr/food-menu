4f3bae371f78160c735af98ee743d407c0777758
sajjad <sajjadzolghadr777@gmail.com>
2026-07-11 13:33:03 +0330

    feat: configure profile image upload and default avatar

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as authentication_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('food.urls')),
@@ -26,3 +29,7 @@ urlpatterns = [
     path('logout/',authentication_views.LogoutView.as_view(next_page='food:index'),name='logout'),
     path('profile/',user_views.profilepage,name='profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
